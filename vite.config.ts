4054b8515182bbec8a56c5c6af980bd4b5443c04
Danyiel Colin <danyiel5978@gmail.com>
2025-11-08 21:35:42 -0600

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/adx420-frontend/' : '/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
